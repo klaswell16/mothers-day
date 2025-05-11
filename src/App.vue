e401b41e0cad4f8a5d4b2828877c63455bb44e8e
Kayan Laswell <kcclaswell@gmail.com>
2025-05-11 17:22:13 -0400

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+transition = false
+
+</script>
+
+<template>
+  <header>
+    working
+  </header>
+  <div>
+    <button @click="transition = true">click me</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
