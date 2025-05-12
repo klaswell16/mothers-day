d46369a64c686b7a70888f14128052dff065b0dc
Kayan Laswell <kcclaswell@gmail.com>
2025-05-12 15:23:30 -0400

    overlay button on top of image

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,54 @@
 <script setup>
 import { ref } from 'vue';
-const transition = ref(false)
-
+const transition = ref(false);
 </script>
 
 <template>
-  <header>
-    working
-  </header>
-  <div>
-    <button @click="transition = true">click me</button>
+  <header>working</header>
+  <div class="image-container">
+    <img
+      class="card-image"
+      alt="boys waiting"
+      src="/boys_waiting.png"
+    />
+    <button 
+      class="overlay-button" 
+      @click="transition = true"
+    >
+      click me
+    </button>
   </div>
-  <div v-if="transition === true">
-    trans
-  </div>
+  <div v-if="transition">trans</div>
 </template>
 
 <style scoped>
+.image-container {
+  position: relative; 
+  display: inline-block; 
+}
 
+.card-image {
+  height: 800px;
+  width: 1200px;
+  display: block;
+}
+
+.overlay-button {
+  position: absolute;
+  bottom: 55%; 
+  right: 50%;  
+  
+  
+  background: transparent;
+  border: 2px solid black;
+  color: white;
+  padding: 10px 20px;
+  cursor: pointer;
+  font-size: 16px;
+  border-radius: 5px;
+}
+
+.overlay-button:hover {
+  background: rgba(255, 255, 255, 0.2);
+}
 </style>
